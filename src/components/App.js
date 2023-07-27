93c08cbab170d9530dc1fe25a9df2383ba097735
Linet <muthiigathoni@gmail.com>
2023-07-27 10:13:30 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
@@ -7,7 +7,36 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+
+  const [activePage, setActivePage] = useState("home")
+
+  const containerStyle = {
+    marginTop: '20px',
+  };
+
+  const navbarStyle = {
+    backgroundColor: '#007bff',
+    color: '#fffg',
+    padding: '10px',
+    textColor: '#fffg'
+  };
+
+  const handleNavClick = (page) => {
+    setActivePage(page)
+  }
+  return (
+    <div style={navbarStyle} className="container mt-3">
+    <NavBar activePage={activePage} handleNavClick={handleNavClick}/>
+    <div style={containerStyle}>
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+        <Route path="/" component={Home} />
+      </Switch>
+      </div>
+      </div>
+  );
 }
 
 export default App;
